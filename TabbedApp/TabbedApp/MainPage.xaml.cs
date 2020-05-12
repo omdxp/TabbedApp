057264acb01ee4577333a71f2d665cd206fa739d
Omar-Belghaouti <bel_omar18@yahoo.com>
2020-05-12 15:42:39 +0200

    Add project files.

---
+++ TabbedApp/TabbedApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TabbedApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoTeSecondPage(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NavigationPage(new TabbedPage1()));
+        }
+    }
+}
---
+++ TabbedApp/TabbedApp/TabbedPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPage1 : TabbedPage
+    {
+        public TabbedPage1()
+        {
+            InitializeComponent();
+        }
+
+        private void Show(object sender, EventArgs e)
+        {
+            LabelTab1.Text = EntryTab1.Text;
+        }
+    }
+}
